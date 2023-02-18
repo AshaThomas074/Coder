4fa1398f0e1ea7bd303efd6f46d4200451cd1f83
AshaThomas074 <ashathomas74@gmail.com>
2023-02-18 23:05:35 +0000

    Add project files.

---
+++ Coder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
