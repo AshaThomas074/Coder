28ee8a7b839f425fd83bedef6db555fc9d3f5799
AshaThomas074 <ashathomas74@gmail.com>
2023-03-16 15:59:03 +0000

    design change

--- Coder/wwwroot/js/site.js
+++ Coder/wwwroot/js/site.js
@@ -3,3 +3,31 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    $("#spnValid").hide();
+
+    $("#btnContestSubmit").on("click", function () {
+        if ($("#ddlQuestion").val() == "") {
+            $("#ddlQuestion").addClass("input-validation-error");
+            $("#ddlQuestion").attr("aria-describedby", "Difficulty-error");
+            $("#ddlQuestion").attr("aria-invalid", "true");
+            $("#spnValid").show();
+            return false;
+        }
+    });
+
+    $("#ddlQuestion").on("change", function () {
+        if ($("#ddlQuestion").val() == "") {
+            $("#ddlQuestion").addClass("input-validation-error");
+            $("#ddlQuestion").attr("aria-describedby", "Difficulty-error");
+            $("#ddlQuestion").attr("aria-invalid", "true");
+            $("#spnValid").show();
+            return false;
+        }
+        else {
+            $("#spnValid").hide();
+        }
+    });
+
+    
+});
